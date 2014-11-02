77207a2d7711efa3242d05601038bed190bfdf8e
Ernest Gu <erniewarrensuper@gmail.com>
2014-11-02 15:53:07 -0800

    Added headers from (old) library

---
+++ RobotC/Headers/Bluetooth.h
@@ -0,0 +1,12 @@
+#ifndef BLUETOOTH_H
+#define BLUETOOTH_H
+#pragma systemFile
+
+
+
+// CODE!!! :D
+
+
+
+#include "..\Libraries\Bluetooth.c"
+#endif // BLUETOOTH_H
---
+++ RobotC/Headers/Sensor.h
@@ -0,0 +1,12 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+#pragma systemFile
+
+
+
+// CODE!!! :D
+
+
+
+#include "..\Libraries\Sensor.c"
+#endif // SENSOR_H
