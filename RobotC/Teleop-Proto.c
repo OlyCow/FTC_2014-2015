a7904cdd05bf961cc5eb75af69079d66e857895c
Ernest Gu <erniewarrensuper@gmail.com>
2014-11-08 16:38:31 -0800

    teleop bugfixes

--- RobotC/Teleop-Proto.c
+++ RobotC/Teleop-Proto.c
@@ -5,7 +5,7 @@
 #pragma config(Motor,  motorC,          motor_clamp_R, tmotorNXT, PIDControl, encoder)
 #pragma config(Motor,  mtr_S1_C2_1,     motor_L,       tmotorTetrix, openLoop, reversed)
 #pragma config(Motor,  mtr_S1_C2_2,     motor_pickup,  tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_1,     motor_lift_A,  tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C3_1,     motor_lift_A,  tmotorTetrix, openLoop, reversed, encoder)
 #pragma config(Motor,  mtr_S1_C3_2,     motor_lift_B,  tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C4_1,     motor_R_A,     tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C4_2,     motor_R_B,     tmotorTetrix, openLoop, reversed)
@@ -25,10 +25,16 @@ task main()
 	int power_lift = 0;
 	int power_pickup = 0;
 	int power_clamp = 0;
+	int dump_position = 0;
+
+	const int dump_pos_open = 190;
+	const int dump_pos_closed = 240;
 
 	bool isPickingUp = false;
 
 	Motor_ResetEncoder(motor_lift_A);
+
+	bDisplayDiagnostics = false;
 
 	Joystick_WaitForStart();
 
@@ -38,15 +44,14 @@ task main()
 		power_L = Joystick_GenericInput(JOYSTICK_L, AXIS_Y, CONTROLLER_1);
 		power_R = Joystick_GenericInput(JOYSTICK_R, AXIS_Y, CONTROLLER_1);
 		power_lift = Joystick_GenericInput(JOYSTICK_L, AXIS_Y, CONTROLLER_2);
-		power_lift /= 1;
-		if (Motor_GetEncoder(motor_lift_A)<0) {
+		if (Motor_GetEncoder(motor_lift_A)>-200) {
 			if (power_lift<0) {
 				power_lift = 0;
 			}
-		} else if (Motor_GetEncoder(motor_lift_A)>0) {
-			if (power_lift>0) {
-				power_lift = 0;
-			}
+		//} else if (Motor_GetEncoder(motor_lift_A)>0) {
+		//	if (power_lift>0) {
+		//		power_lift = 0;
+		//	}
 		}
 		if (Joystick_ButtonReleased(BUTTON_X, CONTROLLER_1)) {
 			isPickingUp = !isPickingUp;
@@ -56,12 +61,18 @@ task main()
 		} else {
 			power_pickup = 0;
 		}
-		if (Joystick_Direction(DIRECTION_F)) {
+		if (Joystick_Direction(DIRECTION_F, CONTROLLER_2)) {
 			power_clamp = -100;
-		} else if (Joystick_Direction(DIRECTION_B)) {
+		} else if (Joystick_Direction(DIRECTION_B, CONTROLLER_2)) {
 			power_clamp = 100;
 		} else {
 			power_clamp = 0;
+		}
+
+		if (Joystick_Button(BUTTON_Y) == true) {
+			dump_position = dump_pos_open;
+		} else {
+			dump_position = dump_pos_closed;
 		}
 
 		Motor_SetPower(power_L, motor_L);
@@ -73,5 +84,10 @@ task main()
 		Motor_SetPower(power_pickup, motor_assist);
 		Motor_SetPower(power_clamp, motor_clamp_L);
 		Motor_SetPower(power_clamp, motor_clamp_R);
+
+		Servo_SetPosition(servo_dump, dump_position);
+
+		nxtDisplayCenteredBigTextLine(2, "pwr: %d", power_lift);
+		nxtDisplayCenteredBigTextLine(4, "pos: %d", Motor_GetEncoder(motor_lift_A));
 	}
 }
