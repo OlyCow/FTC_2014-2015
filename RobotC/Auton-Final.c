d482f124b02835daee45e7d66f87cf1322a16495
Ernest Gu <erniewarrensuper@gmail.com>
2014-12-28 17:57:04 -0800

    robot stuff?

---
+++ RobotC/Auton-Final.c
@@ -0,0 +1,17 @@
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  none,     none)
+#pragma config(Hubs,  S2, HTMotor,  none,     none,     none)
+#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
+#pragma config(Sensor, S2,     ,               sensorI2CMuxController)
+#pragma config(Motor,  mtr_S1_C1_1,     motor_RB,      tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C1_2,     motor_RT,      tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     motor_LT,      tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_2,     motor_LB,      tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S2_C1_1,     motor_LT,      tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S2_C1_2,     motor_LB,      tmotorTetrix, openLoop)
+
+#include "includes.h"
+
+task main()
+{
+
+}
