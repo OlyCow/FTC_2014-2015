754f3ea06c4a6ab1eda079188b5db7212da045f4
Ernest Gu <erniewarrensuper@gmail.com>
2015-01-07 22:27:44 -0800

    fixed lift again

--- RobotC/Teleop-Final.c
+++ RobotC/Teleop-Final.c
@@ -32,11 +32,10 @@
 #pragma config(Servo,  srvo_S2_C3_6,    servo18,              tServoNone)
 
 #include "includes.h"
+#include "final.h"
 
 task PID();
 task Display();
-
-tMotor encoder_lift = motor_LT;
 
 int lift_pos = 0;
 int lift_target = 0;
@@ -48,9 +47,6 @@ float term_I = 0.0;
 float term_D = 0.0;
 float power_lift = 0.0;
 float power_lift_temp = 0.0;
-
-const int LIFT_BOTTOM 	= 100;
-const int LIFT_TOP		= 8000;
 
 task main()
 {
@@ -67,16 +63,6 @@ task main()
 
 	initializeGlobalVariables();
 
-	const int servo_dump_closed		= 202;
-	const int servo_dump_open		= 171;
-	const int servo_pickup_L_closed	= 128;
-	const int servo_pickup_L_open	= 128;
-	const int servo_pickup_R_closed	= 128;
-	const int servo_pickup_R_open	= 128;
-	const int servo_hopper_bottom	= 98;
-	const int servo_hopper_center	= 94;
-	const int servo_hopper_goal		= 80;
-
 	MotorDirection pickup_direction = DIRECTION_NONE;
 	MotorDirection clamp_direction = DIRECTION_NONE;
 	HopperPos hopper_pos = HOPPER_DOWN;
@@ -86,11 +72,13 @@ task main()
 	float power_pickup	= 0.0;
 	float power_clamp	= 0.0;
 
-	Servo_SetPosition(servo_dump, servo_dump_closed);
-	//Servo_SetPosition(servo_pickup_L, servo_pickup_L_closed);
-	//Servo_SetPosition(servo_pickup_R, servo_pickup_R_closed);
-	Servo_SetPosition(servo_hopper_T, 128 + servo_hopper_bottom);
-	Servo_SetPosition(servo_hopper_B, 128 - servo_hopper_bottom);
+	Servo_SetPosition(servo_dump, pos_servo_dump_closed);
+	Servo_SetPosition(servo_hopper_T, 128 + pos_servo_hopper_down);
+	Servo_SetPosition(servo_hopper_B, 128 - pos_servo_hopper_down);
+	Servo_SetPosition(servo_pickup_L, 127 + pos_servo_pickup_large);
+	Servo_SetPosition(servo_pickup_R, 128 - pos_servo_pickup_large);
+
+	Motor_ResetEncoder(encoder_lift);
 
 	Task_Spawn(PID);
 	Task_Spawn(Display);
@@ -99,8 +87,12 @@ task main()
 	while (true) {
 		Joystick_UpdateData();
 
-		is_lift_manual = true;
 		power_lift_temp = Joystick_GenericInput(JOYSTICK_L, AXIS_Y, CONTROLLER_2);
+		if (abs(power_lift_temp) > 5) {
+			is_lift_manual = true;
+		} else {
+			is_lift_manual = false;
+		}
 
 		power_L = Joystick_GenericInput(JOYSTICK_L, AXIS_Y);
 		power_R = Joystick_GenericInput(JOYSTICK_R, AXIS_Y);
@@ -119,6 +111,20 @@ task main()
 			}
 		}
 
+		if (Joystick_ButtonPressed(BUTTON_X, CONTROLLER_2)) {
+			lift_target = pos_lift_bottom;
+			is_lift_manual = false;
+		} else if (Joystick_ButtonPressed(BUTTON_A, CONTROLLER_2)) {
+			lift_target = pos_lift_low;
+			is_lift_manual = false;
+		} else if (Joystick_ButtonPressed(BUTTON_B, CONTROLLER_2)) {
+			lift_target = pos_lift_medium;
+			is_lift_manual = false;
+		} else if (Joystick_ButtonPressed(BUTTON_Y, CONTROLLER_2)) {
+			lift_target = pos_lift_high;
+			is_lift_manual = false;
+		}
+
 		if (Joystick_Direction(DIRECTION_B, CONTROLLER_2)) {
 			clamp_direction = DIRECTION_IN;
 		} else if (Joystick_Direction(DIRECTION_F, CONTROLLER_2)) {
@@ -128,9 +134,9 @@ task main()
 		}
 
 		if (Joystick_Button(BUTTON_Y)) {
-			Servo_SetPosition(servo_dump, servo_dump_open);
+			Servo_SetPosition(servo_dump, pos_servo_dump_open);
 		} else {
-			Servo_SetPosition(servo_dump, servo_dump_closed);
+			Servo_SetPosition(servo_dump, pos_servo_dump_closed);
 		}
 		if (Joystick_ButtonPressed(BUTTON_X)) {
 			switch (hopper_pos) {
@@ -148,16 +154,16 @@ task main()
 
 		switch (hopper_pos) {
 			case HOPPER_DOWN :
-				Servo_SetPosition(servo_hopper_T, 128 + servo_hopper_bottom);
-				Servo_SetPosition(servo_hopper_B, 128 - servo_hopper_bottom);
+				Servo_SetPosition(servo_hopper_T, 128 + pos_servo_hopper_down);
+				Servo_SetPosition(servo_hopper_B, 128 - pos_servo_hopper_down);
 				break;
 			case HOPPER_CENTER :
-				Servo_SetPosition(servo_hopper_T, 128 + servo_hopper_center);
-				Servo_SetPosition(servo_hopper_B, 128 - servo_hopper_center);
+				Servo_SetPosition(servo_hopper_T, 128 + pos_servo_hopper_center);
+				Servo_SetPosition(servo_hopper_B, 128 - pos_servo_hopper_center);
 				break;
 			case HOPPER_GOAL :
-				Servo_SetPosition(servo_hopper_T, 128 + servo_hopper_goal);
-				Servo_SetPosition(servo_hopper_B, 128 - servo_hopper_goal);
+				Servo_SetPosition(servo_hopper_T, 128 + pos_servo_hopper_goal);
+				Servo_SetPosition(servo_hopper_B, 128 - pos_servo_hopper_goal);
 				break;
 		}
 
@@ -191,15 +197,20 @@ task main()
 		Motor_SetPower(power_pickup, motor_pickup);
 		Motor_SetPower(power_clamp, motor_clamp_L);
 		Motor_SetPower(power_clamp, motor_clamp_R);
+
+		Servo_SetPosition(servo_pickup_L, 127 + pos_servo_pickup_large);
+		Servo_SetPosition(servo_pickup_R, 128 - pos_servo_pickup_large);
+
+		Time_Wait(5);
 	}
 }
 
 task PID()
 {
-	const float kP_up = 0.5;
-	const float kI_up = 0.0;
+	const float kP_up = 0.13;
+	const float kI_up = 0.02;
 	const float kD_up = 0.0;
-	const float kP_down = 0.5;
+	const float kP_down = 0.1;
 	const float kI_down = 0.0;
 	const float kD_down = 0.0;
 	const float I_term_decay_rate = 0.8;
@@ -225,11 +236,11 @@ task PID()
 		lift_pos = Motor_GetEncoder(encoder_lift);
 
 		if (is_lift_manual == false) {
-			if (lift_target<LIFT_BOTTOM) {
-				lift_target = LIFT_BOTTOM;
+			if (lift_target < pos_lift_bottom) {
+				lift_target = pos_lift_bottom;
 			}
-			if (lift_target>LIFT_TOP) {
-				lift_target = LIFT_TOP;
+			if (lift_target > pos_lift_top) {
+				lift_target = pos_lift_top;
 			}
 			error = lift_target - lift_pos;
 			if (error > 0) {
@@ -261,7 +272,7 @@ task PID()
 			lift_target = lift_pos;
 			power_lift = power_lift_temp;
 		}
-		if (abs(power_lift)<5) {
+		if (abs(power_lift)<15) {
 			power_lift = 0;
 		}
 
